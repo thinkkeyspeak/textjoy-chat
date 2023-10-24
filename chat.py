4e8de26795e76eab2db39b3123c9c621aadb9a34
thinkkeyspeak <stevenarho@gmail.com>
2023-10-24 10:24:40 -0400

    added spinner and ai avatar

--- chat.py
+++ chat.py
@@ -1,6 +1,49 @@
 import streamlit as st
 import requests, string, random
 
+def run():
+    # Get account_id from URL query parameters
+    query_params = st.experimental_get_query_params()
+    account_id = query_params.get("account_id", [None])[0]
+    conversation_sid = query_params.get("conversation_sid", [None])[0]
+
+    # Set page title
+    st.set_page_config(
+        page_title="TextJoy Chat",
+        page_icon="static/favicon-transparent.ico",
+    )
+    st.title("Start an order")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        st.session_state.user_id = create_user_id()
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("I'd like a ..."):
+
+        # Add user message to chat history
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # Display assistant response in chat message container
+        with st.chat_message(name="assistant", avatar="static/logo-icon-yellow.jpg"):
+            with st.spinner("Thinking..."):
+                reply, conversation_sid = call_web_handler(account_id, st.session_state.user_id, prompt, conversation_sid)
+            st.markdown(reply)
+
+        st.session_state.messages.append({"role": "assistant", "content": reply})
+        st.session_state.conversation_sid = conversation_sid
+
+
 def create_user_id():
     ''' Create fake number and conversation sid '''
     user_id = "webuser-" + generate_random_string(12)
@@ -31,44 +74,6 @@ def generate_random_string(length=12):
     return ''.join(random.choice(characters) for _ in range(length))
 
 
-# LOAD CHAT CONTENTS
-def run():
-    # Get account_id from URL query parameters
-    query_params = st.experimental_get_query_params()
-    account_id = query_params.get("account_id", [None])[0]
-    conversation_sid = query_params.get("conversation_sid", [None])[0]
-
-    # Set page title
-    st.title("Start an order")
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        st.session_state.user_id = create_user_id()
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # React to user input
-    if prompt := st.chat_input("I'd like a ..."):
-
-        # Add user message to chat history
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown("...")
-            reply, conversation_sid = call_web_handler(account_id, st.session_state.user_id, prompt, conversation_sid)
-            st.markdown(reply)
-
-        st.session_state.messages.append({"role": "assistant", "content": reply})
-        st.session_state.conversation_sid = conversation_sid
-
+# Run the app
 if __name__ == "__main__":
     run()
