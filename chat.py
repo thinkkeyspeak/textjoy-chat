c39046b2e5e580f7fc579a50ba236b59f0e12c97
thinkkeyspeak <stevenarho@gmail.com>
2023-10-25 06:36:17 -0400

    can now download properly formatted jsonl

--- chat.py
+++ chat.py
@@ -9,13 +9,8 @@ BASE_URL = st.secrets["base_url"]
 
 def run(conversation_sid):
     if conversation_sid:
-        all_messages, user_id = get_conversation(conversation_sid)
-        filtered_messages = [
-            message for message in all_messages 
-            if message["role"] == "user" 
-            or (message["role"] == "assistant" and not message["function_call"])
-        ]
-        st.session_state.messages = filtered_messages
+        messages, user_id = get_conversation(conversation_sid)
+        st.session_state.messages = messages
         st.session_state.user_id = user_id
     else:
         st.session_state.messages = []
@@ -23,15 +18,18 @@ def run(conversation_sid):
     if not st.session_state.get("user_id"):
         st.session_state.user_id = create_user_id()
 
-    handle_chat_history()
+    show_chat_history()
     handle_user_input(prompt=st.chat_input("I'd like a ..."), conversation_sid=conversation_sid)
 
 
-def create_user_id():
-    return "webuser-" + generate_random_string(12)
+def show_chat_history():
+    if not dev_mode:
+        st.session_state.messages = [
+            message for message in st.session_state.messages
+            if message["role"] == "user" 
+            or (message["role"] == "assistant" and not message["function_call"])
+        ]
 
-
-def handle_chat_history():
     for message in st.session_state.messages:
         avatars = {
             "assistant": "static/logo-icon-yellow.jpg",
@@ -47,27 +45,47 @@ def handle_chat_history():
 
 
 def manage_message_in_dev_mode(message):
-    
-    message["content"] = st.text_input(
+
+    # Capture edited content
+    edited_content = st.text_input(
         label="Content", 
         value=message.get("content", ""), 
         key=f"content_{message['id']}",
         label_visibility="collapsed"
     )
 
+    # If edited content differs from stored content, update it
+    if edited_content != message["content"]:
+        message["content"] = edited_content
+        update_session_message(message['id'], 'content', edited_content)
+
     if message.get("function_call"):
-        message["function_call"] = st.text_input(
+        edited_function_call = st.text_input(
             label="Function Call",
             value=message.get("function_call", ""), 
             key=f"function_call_{message['id']}"
         )
+        if edited_function_call != message["function_call"]:
+            message["function_call"] = edited_function_call
+            update_session_message(message['id'], 'function_call', edited_function_call)
 
     if message.get("name"):
-        message["name"] = st.text_input(
+        edited_name = st.text_input(
             label="Function Name", 
             value=message.get("name", ""), 
             key=f"name_{message['id']}"
         )
+        if edited_name != message["name"]:
+            message["name"] = edited_name
+            update_session_message(message['id'], 'name', edited_name)
+
+
+def update_session_message(message_id, key, value):
+    """Update a specific message in session state by its ID."""
+    for msg in st.session_state.messages:
+        if msg['id'] == message_id:
+            msg[key] = value
+            break
 
 
 def handle_user_input(prompt, conversation_sid):
@@ -105,6 +123,10 @@ def call_web_handler(account_id, user_id, prompt, conversation_sid=None):
     return data.get('reply'), data.get('conversation_sid')
 
 
+def create_user_id():
+    return "webuser-" + generate_random_string(12)
+
+
 def generate_random_string(length=12):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
@@ -115,15 +137,37 @@ def get_conversation(conversation_sid):
     data = response.json()
     return data.get('chat_history'), data.get('user_id')
 
-def get_selectable_messages(messages):
-    """Convert messages to a format suitable for multi-select."""
-    return [msg["content"] for msg in messages]
 
-def filter_selected_messages(messages, selected_messages_content):
-    """Filter messages based on selected content."""
-    return [msg for msg in messages if msg["content"] in selected_messages_content]
+def format_multiselect_options(messages):
+    """Convert messages to multiselect options."""
+    return[(msg["id"], msg["content"])for msg in messages]
 
-# Initialization and configuration
+
+def format_messages_for_download(selected_options):
+    """Convert selected options to JSONL format."""
+    selected_ids = [option[0] for option in selected_options]
+    messages = [msg for msg in st.session_state.messages if msg["id"] in selected_ids]
+
+    # for each message, return the role and content. If it has a name or function call, include those too
+    formatted_messages = []
+    for msg in messages:
+        formatted_msg = {
+            "role": msg["role"],
+            "content": msg["content"],
+        }
+        if msg.get("name"):
+            formatted_msg["name"] = msg["name"]
+        if msg.get("function_call"):
+            formatted_msg["function_call"] = msg["function_call"]
+        formatted_messages.append(formatted_msg)
+    
+    jsonl = {"messages": formatted_messages}
+    return jsonl
+
+
+# INITIALIZE AND CONFIGURE APP
+
+# Get query parameters
 query_params = st.experimental_get_query_params()
 account_id = query_params.get("account_id", [None])[0]
 conversation_sid = query_params.get("conversation_sid", [None])[0]
@@ -134,35 +178,35 @@ st.set_page_config(
     page_icon="static/favicon-transparent.ico"
 )
 st.session_state.messages, st.session_state.user_id = get_conversation(conversation_sid)
+st.session_state.selected_messages = []
+
 st.title("Start an order")
 
-if not st.session_state.get("selected_messages"):
-    st.session_state.selected_messages = []
-
+# Create sidebar if dev_mode is enabled
 if dev_mode:
 
-    # Create sidebar
     st.sidebar.title("Developer Mode")
 
     # Using multi-select for message selection
-    selectable_messages = get_selectable_messages(st.session_state.messages)
-    selected_messages_content = st.sidebar.multiselect(
-        "Select Messages", selectable_messages, default=[]
+    options = format_multiselect_options(st.session_state.messages)
+    selected_options = st.sidebar.multiselect(
+        "Select Messages", options, format_func=lambda o: o[1]
     )
+    print(selected_options)
 
     # Show count of selected messages
-    st.sidebar.header(f"Selected messages ({len(selected_messages_content)})")
+    st.sidebar.header(f"Selected messages ({len(selected_options)})")
 
     # Offer download for selected messages
-    if selected_messages_content:
-        selected_msgs = filter_selected_messages(st.session_state.messages, selected_messages_content)
+    if selected_options:
+        jsonl = format_messages_for_download(selected_options)
         st.sidebar.download_button(
             label="Download JSONL",
-            data=json.dumps(selected_msgs),
-            file_name="selected_messages.jsonl",
+            data=json.dumps(jsonl),
+            file_name=f"{conversation_sid}.jsonl",
             mime="application/jsonl"
         )
 
-# App execution
+# RUN APP
 if __name__ == "__main__":
     run(conversation_sid)
